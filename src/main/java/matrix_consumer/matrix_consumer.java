f73798d7f09e90591a3318ec78e88e492682d05e
Starcom <Starcom@DESKTOP-SE69V6U>
2017-12-22 01:07:07 +0900

    std2

--- src/main/java/matrix_consumer/matrix_consumer.java
+++ src/main/java/matrix_consumer/matrix_consumer.java
@@ -20,10 +20,12 @@ import kafka.message.MessageAndMetadata;
 
 public class matrix_consumer {
 
-	private static final String TOPIC = "gpu1";
+	private static final String TOPIC = "std2";
 	private static final int NUM_THREADS = 1;
 
 	// static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
+	
+	
 	
 	public static void main(String[] args) throws Exception {
 
