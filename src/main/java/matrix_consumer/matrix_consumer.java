7c9f835cc8b176c52f615e937524cebff394e612
Starcom <Starcom@DESKTOP-SE69V6U>
2017-12-22 00:40:34 +0900

    sh add

--- src/main/java/matrix_consumer/matrix_consumer.java
+++ src/main/java/matrix_consumer/matrix_consumer.java
@@ -20,7 +20,7 @@ import kafka.message.MessageAndMetadata;
 
 public class matrix_consumer {
 
-	private static final String TOPIC = "cpu2500";
+	private static final String TOPIC = "gpu1";
 	private static final int NUM_THREADS = 1;
 
 	// static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
